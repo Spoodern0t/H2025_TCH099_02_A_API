d3344962f805df0ea3f857e9a83170a35653b4d6
VincentMoreauBenoit <123586590+VincentMoreauBenoit@users.noreply.github.com>
2025-04-09 12:17:01 -0400

    Version pour tester azure

--- api/models/element.php
+++ api/models/element.php
@@ -10,15 +10,38 @@
             $this->global = new GlobalMethode();
         }
 
-
+        // TODO reparler de la requetes
         function creerElement($id_calendrier){
             header('Content-type: application/json');
             $data = json_decode(file_get_contents("php://input"), true);
             $pdo = $this->global->getPdo();
 
-            echo $id_calendrier;
+            $token = $data['token'];
+            $nom = $data['nom'];
+            $description = $data['description'];
+            $evenement = $data['evenement'];
+            $id_evenement = $evenement['id'];
 
-            
+            $id_utilisateur = $this->global->verifierToken($token);
+            if(!$id_utilisateur){
+                echo json_encode(["token" => false]);
+                return;
+            }
+
+            try{
+                $pdo->beginTransaction();
+
+
+
+                $pdo->commit();
+            }catch(\Throwable $e){
+                $pdo->rollback();
+                echo json_encode(["token" => false]);
+            }
+
+
+            echo $id_evenement;
+
         }
     }
 
